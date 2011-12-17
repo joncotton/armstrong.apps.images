ae8166fadb7866a45349e3b981d1d2343a93cb8a
Jon Cotton <jcotton@baycitizen.org>
2011-12-16 16:38:11 -0800

    Add trailing slashes to URLs and remove unused imports.

--- armstrong/apps/images/urls.py
+++ armstrong/apps/images/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls.defaults import *
-from django.contrib.auth.decorators import permission_required
+from django.conf.urls.defaults import patterns, url
 
-from .models import Image
 from .views import RenderThumbnail
 
 
 urlpatterns = patterns('',
-    url(r'^thumbnail/(?P<pk>\d+)/original$',
-            RenderThumbnail.as_view(), name='images_render_original'),
-    url(r'^thumbnail/(?P<pk>\d+)/(?P<geometry>[0-9x]*)$',
-            RenderThumbnail.as_view(), name='images_render_thumbnail'),
+    url(r'^thumbnail/(?P<pk>\d+)/original/$',
+            RenderThumbnail.as_view(),
+            name='images_render_original'
+        ),
+    url(r'^thumbnail/(?P<pk>\d+)/(?P<geometry>[0-9x]*)/$',
+            RenderThumbnail.as_view(),
+            name='images_render_thumbnail'
+        ),
 )
